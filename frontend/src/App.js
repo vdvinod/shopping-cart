e6705dee3b71df49d7e385622be55f542b264f77
TChand22 <chand.tanuja86@gmail.com>
2021-02-03 09:07:21 +0530

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
-
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 const App = () => {
   return (
     <>
-      Welcome
+    <Header />
+    <main className="py-3">
+      <Container>
+        Welcome
+      </Container>
+      
+    </main>
+    <Footer />
     </>
   );
 }
