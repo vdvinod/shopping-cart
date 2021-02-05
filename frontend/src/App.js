f1972c6982cfa2c69ca79fe91164c35549f097c8
TChand22 <chand.tanuja86@gmail.com>
2021-02-05 09:29:51 +0530

    added product details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+
 import { Container } from 'react-bootstrap';
+import ProductScreen from './screens/ProductScreen';
 const App = () => {
   return (
-    <>
-    <Header />
-    <main className="py-3">
-      <Container>
-        Welcome
-      </Container>
-      
-    </main>
-    <Footer />
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path='/' component={HomeScreen} exact/>
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
